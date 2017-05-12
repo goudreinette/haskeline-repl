00bfa2ca08917c555abf6dc90f3cad824de39308
reinvdwoerd <reinvanderwoerd@me.com>
2017-05-12 13:51:15 +0200

    Custom commands

--- src/System/Console/Repl.hs
+++ src/System/Console/Repl.hs
@@ -1,26 +1,39 @@
 module System.Console.Repl where
 
+import           Control.Monad
 import           Control.Monad.Trans
+import           Safe
 import           System.Console.ANSI
 import           System.Console.Haskeline
 
 type Prompt = String
 type InputLine = String
+type Command a = (String, InputLine -> IO Bool)
+
+defaultCommands :: [Command a]
+defaultCommands =
+  [(":q", const $ return False)]
+
 
 repl :: Prompt -> (InputLine -> IO a) -> IO ()
 repl prompt f =
+  replWith prompt f defaultCommands
+
+
+replWith :: Prompt -> (InputLine -> IO a) -> [Command a] -> IO ()
+replWith prompt f commands =
   runInputT defaultSettings loop
-  where
-    loop = do
-      line <- getInputLine prompt
-      case line of
-        Just input -> do
-          liftIO $ f input
-          loop
-        Just ":q" ->
-          return ()
-        Nothing ->
-          return ()
+  where loop =
+          getInputLine prompt >>= maybe (return ()) pickCommand
+
+        pickCommand input =
+          case headMay (words input) >>= (`lookup` commands)  of
+            Just cmd -> do
+              continue <- liftIO $ cmd input
+              when continue loop
+            Nothing -> do
+              liftIO $ f input
+              loop
 
 
 printError err = do
